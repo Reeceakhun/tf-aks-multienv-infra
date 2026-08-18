210f550f66fc5e96761c0a16afc624be81524ae8
Reece Sabo <reeceakhun@gmail.com>
2026-08-19 01:03:00 +0200

    Add AKS cluster resource

--- main.tf
+++ main.tf
@@ -9,3 +9,31 @@ resource "azurerm_resource_group" "main" {
     managed-by  = "terraform"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "aks-${var.environment}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = var.environment
+    managed-by  = "terraform"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      default_node_pool[0].node_count,
+    ]
+  }
+}
