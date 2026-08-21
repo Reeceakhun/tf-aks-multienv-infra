aec9e1bdb715e05aff04012bef577fdf600c1663
Reece Sabo <reeceakhun@gmail.com>
2026-08-22 00:50:52 +0200

    Fix duplicate backend block and Terraform OIDC auth

--- provider.tf
+++ provider.tf
@@ -19,11 +19,3 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateaksmultienv"
-    container_name        = "tfstate"
-    key                    = "dev.terraform.tfstate"
-  }
-}
