5aabc55817345c38e28934e35ccd3662778c5df6
Reece Sabo <reeceakhun@gmail.com>
2026-08-23 01:31:32 +0200

    Explicitly enable OIDC issuer on AKS cluster (cannot be disabled once set)

--- main.tf
+++ main.tf
@@ -16,6 +16,8 @@ resource "azurerm_kubernetes_cluster" "main" {
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "aks-${var.environment}"
 
+  oidc_issuer_enabled = true
+
   default_node_pool {
     name       = "default"
     node_count = var.node_count
